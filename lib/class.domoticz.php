b3639e26ca1a885ecb984be3bf8f354f20bb7342
ml17950 <ml17950@gmail.com>
2024-05-01 07:17:19 +0200

    sendRequest + setValue

--- lib/class.domoticz.php
+++ lib/class.domoticz.php
@@ -1,9 +1,48 @@
 <?php
 class ClassDomoticz {
-	protected $domo_ip   = '';
+	protected $hostaddr   = '';
 
-	public function __construct($domo_ip = null) {
-		if ($domo_ip === null) return false;
-		$this->domo_ip = $domo_ip;
+	public function __construct($hostaddr = null) {
+		if ($hostaddr === null) return false;
+		$this->hostaddr = $hostaddr;
+	}
+
+	public function sendRequest($url) {
+		$header = array('Accept: */*',
+						'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0',
+						'Content-Type: application/json',
+						'X-Requested-With: XMLHttpRequest',
+						'Connection: keep-alive',
+						'Pragma: no-cache',
+						'Cache-Control: no-cache');
+
+		$options = array(
+			'http' => array(
+				'timeout' => 5,
+				'method'  => 'GET',
+				'header'  => $header
+			),
+			'ssl' => array(
+				'verify_peer'      => false,
+				'verify_peer_name' => false
+			)
+		);
+
+		$response = file_get_contents($url, false, stream_context_create($options));
+
+		return json_decode($response, true);
+	}
+
+	public function setValue($id, $nvalue = 0, $svalue = '') {
+		$url = 'http://'.$this->hostaddr.'/json.htm?type=command&param=udevice&idx='.$id.'&nvalue='.$nvalue.'&svalue='.$svalue;
+
+		$response = $this->sendRequest($url);
+
+		print_r($response);
+
+		if ($response['status'] != 'OK')
+			return false;
+
+		return true;
 	}
 }
